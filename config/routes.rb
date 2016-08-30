8485485df91e74025ee47eb4836905ecfd9d4929
Alex Welch <me@alexwelch.com>
2016-08-29 19:36:21 -0600

    Bring over Julie's work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :registrations
-  root to: 'registrations#index'
+  resources :registrations, only: [:new, :create]
+  root to: 'registrations#new'
 end
