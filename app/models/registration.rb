66002088ed0319720d70779a18440b0415192fbc
Alex Welch <me@alexwelch.com>
2016-08-31 19:43:51 -0600

    validate registrations

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,16 @@
 class Registration < ApplicationRecord
-  validates :email, presence: true
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Not a valid email address" }
+  validates :name, presence: true
+  validates :attending, inclusion: { in: [true, false], message: "- Please select yes or no" }
+
+  before_save do
+    if attending?
+      self.total_guests = 1 if total_guests.nil?
+    else
+      self.total_guests = 0
+    end
+  end
 end
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
-* validations
-* fix divs w/in dls
-* validate html
-* display flash notices
-* send confirmation email (optional)
-* display page for registrations (optional)
-* finish styling form
+* validations - Alex
+* fix divs w/in dls - Julie
+* finish styling form - Julie and Alex
+* validate html - Julie
+* display flash notices - Julie and Alex
+* send confirmation email (optional) - Alex and Julie
+* display page for registrations (optional) - Alex
